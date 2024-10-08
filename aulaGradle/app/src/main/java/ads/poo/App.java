4504a7682a582f41c78cc4bbec8a3d262087a996
Gean Luccas <gean.x@aluno.ifsc.edu.br>
2024-10-08 20:19:11 -0300

    Aula sobre ferrramentas da IDE

--- aulaGradle/app/src/main/java/ads/poo/App.java
+++ aulaGradle/app/src/main/java/ads/poo/App.java
@@ -12,21 +12,30 @@ public class App {
 
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+        Scanner teclado = new Scanner(System.in);
         int[] moedas = new int[6];
+        String[] textoMoeda = {"0,01", "0,05", "0,10", "0,25", "0,50", "1,00"};
+        int entrada = 0;
+        double total = 0;
 
-        System.out.println("Você tem moedas no bolso ? Se sim , digite uma por uma: ");
-        int moeda = scanner.nextInt();
+        do {
+            System.out.println("Qual o valor da moeda: ");
+            entrada = teclado.nextInt();
+            switch (entrada) {
+                case 1 -> {moedas[0]++;total++;}
+                case 5 -> {moedas[1]++;total+=5;}
+                case 10 -> {moedas[2]++;total+=10;}
+                case 25 -> {moedas[3]++;total+=25;}
+                case 50 -> {moedas[4]++;total+=50;}
+                case 100 -> {moedas[5]++;total+=100;}
+                default -> System.out.println("Valor incorreto");
+            }
+        }while(entrada != 0);
 
-        switch (moeda){
-            case 1:
-                moedas[0] ++; //moedas de 1 centavo
-                break;
-            case 2:
-                moedas[1] ++;  //moedas de 5 centavo
-                break;
-                case 3:
+        System.out.println(String.format("Você tem R$ %8.2f", (total/100)));
+        for(int i = 0; i < 6; i++){
+            String s = String.format("%3d moedas de %s", moedas[i], textoMoeda[i]);
+            System.out.println(s);
         }
-
     }
 }
